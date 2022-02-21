087b9e32993ce7d1e43476cc59a6441b21bdde8a
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-02-21 16:20:46 +0700

    update

--- migrations/20220219145812-create-book.js
+++ migrations/20220219145812-create-book.js
@@ -22,11 +22,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       }
     });
   },
